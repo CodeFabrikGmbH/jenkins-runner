c5ab058a784837e32b60e7889e4c88fbe5dbe09d
abarthol <7668433+abarthol@users.noreply.github.com>
2020-08-26 09:55:34 +0200

    Update jenkins-slave.sh
    * move handling of docker group to start script
    * improve handling of OPTIONS parameter

--- jenkins-slave.sh
+++ jenkins-slave.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env sh
 
+DOCKER_GROUP=${DOCKER_GROUP:?Please provide the docker group}
+
+sudo addgroup --system -gid $DOCKER_GROUP docker && sudo usermod -aG docker jenkins
+
 mkdir $HOME/.jenkins | true
 
-java -jar /tmp/slave.jar "$OPTIONS" -jnlpUrl "$JNPL_URL" -secret "$SECRET" -workDir "$WORKDIR"
+if [ -z $OPTIONS ]; then
+ java -jar /tmp/slave.jar -jnlpUrl "$JNPL_URL" -secret "$SECRET" -workDir "$WORKDIR"
+else
+ java -jar /tmp/slave.jar "$OPTIONS" -jnlpUrl "$JNPL_URL" -secret "$SECRET" -workDir "$WORKDIR"
+fi
